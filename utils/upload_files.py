aafcadb86d0a9d43643a050703036842784598ff
Argenis Vargas <argenis.vargas@crehana.com>
2023-11-18 16:58:35 -0500

    redefine upload file functions

--- utils/upload_files.py
+++ utils/upload_files.py
@@ -9,7 +9,11 @@ from config.enviroment_vars import settings
 def upload_user_image_profile(obj, filename):
     f, ext = os.path.splitext(filename)
     _uuid = uuid.uuid1()
-    url = "user-image-profile/%(uuid)s/%(name)s" % {"uuid": _uuid.time_low, "name": f}
+    url = "user-image-profile/%(uuid)s/%(name)s%(ext)s" % {
+        "uuid": _uuid.time_low,
+        "name": f,
+        "ext": ext,
+    }
     if settings.DEBUG:
         url = "test/%s" % url
     return url
@@ -18,7 +22,11 @@ def upload_user_image_profile(obj, filename):
 def upload_attachment_file(obj, filename):
     f, ext = os.path.splitext(filename)
     _uuid = uuid.uuid1()
-    url = "user-attachment/%(uuid)s/%(name)s" % {"uuid": _uuid.time_low, "name": f}
+    url = "user-attachment/%(uuid)s/%(name)s%(ext)s" % {
+        "uuid": _uuid.time_low,
+        "name": f,
+        "ext": ext,
+    }
     if settings.DEBUG:
         url = "test/%s" % url
     return url
