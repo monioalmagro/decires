7ba233c11acc504f7a4b17708832526f671988df
Argenis Vargas <argenis.vargas@crehana.com>
2023-11-27 21:46:27 -0500

    added changes un psychology

--- apps/psychology/schema/queries.py
+++ apps/psychology/schema/queries.py
@@ -2,8 +2,23 @@
 import strawberry
 
 # Own Libraries
+from apps.psychology.adapters.carreer import CarreerAdapter
 from apps.psychology.adapters.user import UserAdapter
+from apps.psychology.schema.types.carreer import CarreerSelect2Type
 from apps.psychology.schema.types.user import ProfessionalType
+
+
+@strawberry.type()
+class Select2Queries:
+    @strawberry.field()
+    async def carreers(self) -> list[CarreerSelect2Type]:
+        adapter = CarreerAdapter()
+        if results := await adapter.get_objects():
+            return [
+                CarreerSelect2Type.from_db_model(instance=carreer)
+                for carreer in results
+            ]
+        return []
 
 
 @strawberry.type()
