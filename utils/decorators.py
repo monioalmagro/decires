7e3d6369a325af0893f28a8bb4e694e9310446ea
Argenis Vargas <argenis.vargas@crehana.com>
2023-12-28 12:36:23 -0500

    refactor decorator handler

--- utils/decorators.py
+++ utils/decorators.py
@@ -4,8 +4,37 @@ import logging
 
 # Third-party Libraries
 from django.db import DatabaseError, IntegrityError
+from pydantic import ValidationError
+
+# Own Libraries
+from utils.graph.responses import (
+    ResponseIntegrityError,
+    ResponseInternalError,
+    ResponseValidationError,
+)
 
 logger = logging.getLogger(__name__)
+
+
+async def pydantic_error_handler(exp: ValidationError) -> str:
+    """
+    Decode Pydantic ValidationError and extract detailed error messages.
+
+    Parameters:
+    - exp (ValidationError): Pydantic ValidationError instance.
+
+    Returns:
+    str: A formatted string containing detailed error messages.
+    """
+    try:
+        field_errors = exp.errors()
+        error_messages = [
+            f"{field['loc'][0]}: {field['msg']}" for field in field_errors
+        ]
+        return ", ".join(error_messages)
+    except Exception as e:
+        logger.error(f"Error in pydantic_error_handler: {str(e)}")
+        return "Error decoding Pydantic error message"
 
 
 def mutation_exception_handler(log_tag: str):
@@ -31,24 +60,32 @@ def mutation_exception_handler(log_tag: str):
             try:
                 result = await func(*args, **kwargs)
                 return result
-            except AssertionError as exp:
+            except (AssertionError, ValidationError) as exp:
+                if isinstance(exp, ValidationError):
+                    message = await pydantic_error_handler(exp)
+                else:
+                    message = str(exp)
+
                 logger.warning(
                     f"*** {full_log_tag}, VALIDATION ERROR"
                     f" {str(exp)} - {repr(exp)}***",
                     exc_info=True,
                 )
+                return ResponseValidationError(message=str(message))
             except (DatabaseError, IntegrityError) as exp:
                 logger.warning(
                     f"*** {full_log_tag}, INTEGRITY ERROR"
                     f" {str(exp)} - {repr(exp)}***",
                     exc_info=True,
                 )
+                return ResponseIntegrityError(message=str(exp))
             except Exception as exp:
                 logger.warning(
                     f"*** {full_log_tag}, INTERNAL ERROR"
                     f" {str(exp)} - {repr(exp)}***",
                     exc_info=True,
                 )
+                return ResponseInternalError(message=str(exp))
 
         return wrapper
 
